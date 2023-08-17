9ffd77e9869f9d2d261aecc9343d816defb5cbc1
Y. Orçun GÖKBULUT <yorcun.gokbulut@stm.com.tr>
2023-08-17 09:49:55 +0300

    CHG: License has been changed from LGPL to BSD 3-Clause. ADD: Static library build support has been added. ADD: Github CI pipeline has been added.

--- Sources/Json.Test.cpp
+++ Sources/Json.Test.cpp
@@ -1,27 +1,17 @@
-/*******************************************************************************
+/****************************************************************
  JsonCpp - Json.Test.cpp
- ------------------------------------------------------------------------------
+ ----------------------------------------------------------------
  Copyright (c) 2022, Yiğit Orçun GÖKBULUT. All rights Reserved.
 
- This library is free software; you can  redistribute it and/or modify it under
- the terms of the GNU  Lesser General  Public License as  published by the Free
- Software Foundation;  either version 2.1  of the License, or  (at your option)
- any later version.
-
- This library is  distributed in the hope  that it will be  useful, but WITHOUT
- ANY WARRANTY; without even the implied  warranty of MERCHANTABILITY or FITNESS
- FOR A PARTICULAR PURPOSE.  See the GNU Lesser General  Public License for more
- details.
-
- You should have received a copy of the GNU Lesser General Public License along
- with this library;  if not, write  to the Free  Software Foundation,  Inc., 51
- Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ Use of this source code is governed by a BSD-style
+ license that can be found in the LICENSE file or at
+ https://developers.google.com/open-source/licenses/bsd
 
  Copyright Owner Information:
   Name: Yiğit Orçun GÖKBULUT
   Contact: orcun.gokbulut@gmail.com
   Github: https://www.github.com/orcun-gokbulut
-*******************************************************************************/
+****************************************************************/
 
 #include "Json.h"
 
